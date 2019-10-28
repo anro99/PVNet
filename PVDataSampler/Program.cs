082847f5ac587a65ccef59da818c7b45e17311d6
anro99 <andreas.rossi@elkiro.de>
2019-10-28 18:41:06 +0100

    Add project files.

---
+++ PVDataSampler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PVDataSampler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] a_args)
+        {
+            if (StartAsApp(a_args))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(true);
+
+                var mainForm = new MainForm();
+                Application.Run(mainForm);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new Service1()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+
+        private static bool StartAsApp(string[] a_args)
+        {
+            if (a_args.Length == 0)
+                return false;
+            return string.Compare(a_args[0], "-app", StringComparison.InvariantCultureIgnoreCase) == 0;
+        }
+    }
+}
